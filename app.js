70fb0b0aa382a410ea412169849f819f1cb844ca
MichiY3 <michiyoy@hotmail.com>
2020-10-20 21:02:45 +0900

    問題５　FizzBuss 15が出ないけど

--- app.js
+++ app.js
@@ -65,6 +65,19 @@ console.log('----------------5. FizzBuzz----------------');
  * と表示するようにしてください。
  */
 
+for (let i = 1; i<= 100; i++)
+	if (i % 3 === 0){
+		console.log('Fizz');
+	}else if( i % 5 === 0){
+		console.log('Buzz');
+	}else if ( i % 15 === 0){
+		console.log('FizzBuss');
+	}else{
+		console.log(i);
+	}
+	
+
+
 console.log('----------------6.九九----------------');
 /**
  * 九九を表示するプログラムを書いてください。
