e3e0be9b8ac5cfdebfb0c5587a47f2f8e23c808a
MichiY3 <michiyoy@hotmail.com>
2020-10-21 19:33:22 +0900

    授業で回答確認

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ let i = 2;
 	console.log('奇数');
 }
 
+// 回答例
+// if文
+// let i = 2;
+// if (i % 2 === 0) {
+//     console.log(`${i}は偶数です`);　
+// } else {
+//     console.log(`${i}は奇数です`);
+// }
+//     // iは偶数です、と表示される
+
+
+
+// Switch文
+// bool型　（真偽値を扱うデータ型）　true,faulseのみ
+
+switch (i % 2 === 0) {
+    case true:
+        console.log(i + 'は偶数です');
+        break;
+    default:
+        console.log(i +  'は奇数です');
+        break;
+}
 
 console.log('----------------2. 合格判定----------------');
 /**
@@ -35,6 +58,33 @@ if (math >= 60 && english >= 60 && math + english >= 140){
 }
 
 
+// 回答例　①
+// let math = 50;
+// let english = 100;
+// let total = math + english;
+// if (math >= 60 && english >= 60 && total >= 140) {
+//   console.log('合格');
+// } else {
+//   console.log('不合格');
+// }
+
+// 回答例　① 書くの途中
+ // let (math >= 60){
+ // 	if(english >=60){
+ // 		if(total >= 140){
+ // 			console.log('合格');
+ // 		}else{
+ 		
+ // 			console.log('合格点が不足で不合格');
+ // 		}
+ // 	}else{
+ // 		console.log('英語点が不足で不合格');
+ // 	}
+ // 	}else{
+ // 		console.log('数学点が不足で不合格');
+ // 	}
+
+
 console.log('----------------3. 数を数える----------------');
 /**
  * 配列numbersの中に数字の3が何個あるか画面に表示するプログラム
@@ -48,8 +98,31 @@ console.log('----------------3. 数を数える----------------');
 // 			console.log('できない。。。');
 // 	}
 
+// 回答例
+
+// ３を見つけた個数を格納しておく変数
+ let res = 0;
+let numbers = [1, 3, 4, 5, 8, 9, 3, 3];
+for (let number of numbers) {
+    if (number === 3) {
+    		// res++; と　res + 1; は同じ処理
+        res++;
+    }
+}
+console.log(res);
 
 
+// ーーーーーーーーー別回答ーーーーーーーーーーーー
+
+for (let i = 0; i <= 9; i++){
+ if(numbers[i] === 3){
+ 	res = res + 1;
+ }
+}
+console.log(res);
+
+
+// let fruits = ['']
 
 console.log('----------------4. 3倍した数を表示する----------------');
 /**
@@ -61,6 +134,12 @@ console.log('----------------4. 3倍した数を表示する----------------');
  	if( i % 3 === 0)
  	console.log(i);
  }
+
+// 回答例
+for (let i = 1; i <= 10; i++){
+	console.log(i * 3);
+}
+ 
 
 console.log('----------------5. FizzBuzz----------------');
 /**
@@ -83,6 +162,38 @@ for (let i = 1; i<= 100; i++)
 		console.log(i);
 	}
 	
+// 回答例　①
+
+ // if (i % 5 === 0 && i % 3 === 0) {
+ //    console.log('FizzBuzz');
+ //  } else if (i % 5 === 0) {
+ //    console.log('Buzz');
+ // }else if ( i % 3 === 0){
+	// 	console.log('Fizz');
+	// }else{
+	// 	console.log(i);
+	// }
+	
+
+// 回答例　②
+
+let moji ="";
+for  ( let i = 1; i<=  100; i++){
+	moji = i;
+	 if (i % 3 === 0 ){
+	 	moji = 'fizz';
+	 }
+	 if (i %5 === 0 ){
+	 	if(moji != 'fizz'){
+	 		moji = '';
+	 	}
+
+	 	moji = moji + 'buzz';
+	 }
+	 console.log(moji);
+	 	}
+
+
 
 
 console.log('----------------6.九九----------------');
@@ -97,17 +208,35 @@ console.log('----------------6.九九----------------');
  	
 
 
+for (let i = 1; i <= 9; i++) {
+	console.log(`${i}の段`)
+  for (let j = 1; j <= 9; j++) {
+    let res = i * j;
+    console.log(`${i}x${j}=${res}`);
+  }
+}
+
 
 console.log('----------------7. 繰り返しの中断、スキップ----------------');
 /**
  * 九九の中で答えが30未満の数値だけ表示してください。
  */
 
- // for (let i = 1; i<=9; i++){
- // 	for( let j = 1; j <=9; j++){
- // 		if ( i * j  > 30 )
- // 		console.log(i * j);
- // 	} }
+
+
+
+// 回答例
+
+ for (let i = 1; i <= 9; i++) {
+ 	console.log(`${i}の段`)
+  for (let j = 1; j <= 9; j++) {
+    let res = i * j;
+    if (res >= 30) {
+      continue;
+    }
+    console.log(res);
+  }
+}
 
 
 // チャレンジ問題
@@ -121,10 +250,31 @@ console.log('----------------1. 女湯問題----------------');
  * 女湯に入れる場合は「入れます」、入れない場合は「入れない」と表示してください。
  */
 
+// 回答例
+ let  customer ={ age: 28, gender : 'male'};
+
+ if (customer['gender'] === 'female'){
+ 	console.log('入れます');
+ }else {
+ 	if (customer['age'] <= 3){
+ 		console.log('入れます');
+ }else{
+ 	console.log('入れません'); }
+ }
+
+
+
+
+
 console.log('----------------2. 素数を表示するプログラム----------------');
 /**
  * 100以下の素数を表示してください。
  * 1は素数には含みません。
  * 素数とは1とその数以外では割り切れない数です。
  * 言い方をかえると約数が2つしかない数です。
+
+
+
+
+
  */
